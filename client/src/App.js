9f72f2fb7a0e2e035ec388deb09bbdebaa4bfc7f
Erica  Cohen <ericacohen@Ericas-MacBook-Air.local>
2018-02-08 13:54:07 -0800

    added panels for basic styling on homepage

--- client/src/App.js
+++ client/src/App.js
@@ -10,9 +10,34 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+        <div class="panel panel-default">
+          <div class="panel-body">
+          <div class="row">
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    Adopt a pet
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    Find a new home for my pet
+                  </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+                    Success stories
+                  </div>
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
