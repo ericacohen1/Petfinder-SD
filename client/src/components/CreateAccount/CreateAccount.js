c9cacfd1adf05a7ac2759b4b022c236e7d60e100
ematt44 <ericmatthews133@gmail.com>
2018-02-13 14:33:50 -0800

    doublearrow

--- client/src/components/CreateAccount/CreateAccount.js
+++ client/src/components/CreateAccount/CreateAccount.js
@@ -3,7 +3,9 @@ import "./CreateAccount.css";
 import { Link } from "react-router-dom";
 
 const CreateAccount = props => (
+       
        <div>
+          <h4>{props.something}</h4>
            <div className="account-container">
            <h2>Create Your Pet Adoption SD Account</h2>
            <h4>Complete the fields below to sign up!</h4>
@@ -12,9 +14,9 @@ const CreateAccount = props => (
              <input type="text" name="name" placeholder="Name" /><br />
              <input type="email" name="email" placeholder="Email" /><br />
              <input type="text" name="password" placeholder="Password" /><br />
-             <input type="text" name="zipcode" placeholder="Zipcode" /><br />
+             {/* <input type="text" name="zipcode" placeholder="Zipcode" /><br /> */}
             
-             <Link to="Home"><button>Submit</button></Link>     
+             <Link to="Home"><button onClick={props.handleFormSubmit("accountData")}>Submit</button></Link>     
              <Link to="LogIntoAccount">Log In</Link>    
            </form>
          </div>
