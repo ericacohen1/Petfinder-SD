e950bd16c36115b03a05833aa298251b685a64c9
ematt44 <ericmatthews133@gmail.com>
2018-02-13 11:31:18 -0800

    input

--- client/src/App.js
+++ client/src/App.js
@@ -68,34 +68,6 @@ class App extends Component {
 
 
 
-       {/* <div class="panel panel-default">
-        <div class="panel-body">
-            <div class="row">
-              <div class="col-md-4">
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                      Adopt a pet
-                    </div>
-                  </div>
-              </div>
-              <div class="col-md-4">
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                      Find a new home for my pet
-                    </div>
-                  </div>
-              </div>
-              <div class="col-md-4">
-                  <div class="panel panel-default">
-                    <div class="panel-body">
-                      Success stories
-                    </div>
-                  </div>
-              </div>
-            </div>
-         </div> 
-      </div> 
-     </div> 
-      */}
+       
 
 export default App;
