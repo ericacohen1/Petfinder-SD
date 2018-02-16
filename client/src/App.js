8ff875e7a10d5c75ef599503e4e520fddaaeaeea
duff.brian21@gmail.com <duff.brian21@gmail.com>
2018-02-16 13:33:19 -0800

    deleted logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
