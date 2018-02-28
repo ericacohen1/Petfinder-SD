e7994d1c521be98b5766f6f1164cdb897fc8cdea
Erica  Cohen <ericacohen@Ericas-MacBook-Air.local>
2018-02-28 11:40:55 -0800

    about text

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -2,13 +2,22 @@ import React from "react";
 import "./About.css";
 
 const About = props => (
-
+  <div className="container">
     <div className="panel panel-default">
   <div className="panel-heading">
     <h3 className="panel-title">About Us</h3>
   </div>
   <div className="panel-body">
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  <p>The goal of PetFinder SD is to keep as many furry friends in the San Diego area out of pounds and shelters as possible. We built this application to connect pet owners who are unable to take care of their pet with people looking to adopt a pet. </p>
+      <h4>Our Mission: </h4>
+      <p>Crete a simple and easy online marketplace for pets in order to: </p>
+      <ol>
+        <li>Increase public awareness of local pets that need a home</li>
+        <li>Increase the overall effectiveness of pet adoption programs in the area</li>
+        <li>Decrease the number of pets sent to shelters</li>
+        <li>Elevate the status of pets to that of family members</li>
+      </ol>
+  </div>
   </div>
 </div>
 )
